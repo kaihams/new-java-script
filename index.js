c84a42b68b3a5afe6c3ecf02db21b10d51ef9491
kaihams <kai.f.hams@gmail.com>
2020-07-28 16:39:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var person = { 
+  name: "kai hams",
+  age: 15,
+
+};
+
+console.log(person)
+
+
+var rectangle = {
+  width: 23, height: 23
+}
+console.log(rectangle);
+
+var x;{
+x = 7;
+}
+
+x += 63;
